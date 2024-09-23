b52212974b190e5e22ba97f9cca2d1a38eaacd98
wanglongkai <13699283312@163.com>
2024-09-23 15:59:05 +0800

    初始化项目

---
+++ App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+	import {onLaunch, onShow, onHide} from '@dcloudio/uni-app'
+	
+	onLaunch(() => {
+		console.log('App Launch')
+	})
+	
+	onShow(() => {
+		console.log('App Show')
+	})
+	onHide(() => {
+		console.log('App Hide')
+	})
+</script>
+
+<style scoped lang="scss">
+	/*每个页面公共css */
+</style>
