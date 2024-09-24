fd121538930ae7f76e49b8d438ff72df1ff3cc95
wanglongkai <13699283312@163.com>
2024-09-24 11:50:15 +0800

    ci: config.json

--- http/request.ts
+++ http/request.ts
@@ -8,59 +8,60 @@ type TMethod = "GET" | "OPTIONS" | "HEAD" | "POST" | "PUT" | "DELETE" | "TRACE" 
 
 const manager = new RequestManager()
 
-const baseRequest = async (url: string, method: TMethod, data = {}, loading = true) => {
+const baseRequest = async (url : string, method : TMethod, data = {}, loading = true) => {
 	let requestId = manager.generateId(method, url, data)
 	if (!requestId) {
 		console.log('重复请求')
 		return false
 	}
-	const header: any = {}
+	const header : any = {}
 	header.token = getStorageSync('token') || '';
 
 	loading && uni.showLoading({ title: 'loading' });
 
 	return new Promise((resolve, reject) => {
-			uni.request({
-				url: BASE_URL + url,
-				method: method || 'GET',
-				header: header,
-				timeout: 10000,
-				data: data || {},
-				complete: () => {
-					manager.deleteById(requestId as string)
-				},
-				success: (successData) => {
-					const res: any = successData.data
-					if (successData.statusCode == 200) {
-						if (res.resultCode == 'PA-G998') {
-							clearStorageSync()
-							useRouter('/pages/login/index', 'reLaunch')
-						} else {
-							resolve(res.data)
-						}
+		uni.request({
+			url: BASE_URL + url,
+			method: method || 'GET',
+			header: header,
+			timeout: 10000,
+			data: data || {},
+			complete: () => {
+				manager.deleteById(requestId as string)
+			},
+			success: (successData) => {
+				const res : any = successData.data
+				if (successData.statusCode == 200) {
+					if (res.resultCode == 'PA-G998') {
+						clearStorageSync()
+						useRouter('/pages/login/index', 'reLaunch')
 					} else {
-						toast('数据错误，请检查响应结构')
-						reject(res)
+						resolve(res.data)
 					}
-				},
-				fail: (msg) => {
-					toast('网络连接失败，请稍后重试')
-					reject(msg)
+				} else {
+					toast('数据错误，请检查响应结构')
+					reject(res)
 				}
-			})
-		}).finally(() => {
+			},
+			fail: (msg) => {
+				toast('网络连接失败，请稍后重试')
+				reject(msg)
+			}
+		})
+	}).finally(() => {
 		uni.hideLoading()
 	});
 }
 
 
-const request: Record<TMethod, Function> = {} as Record<TMethod, Function>;
+const request : Record<TMethod, Function> = {} as Record<TMethod, Function>;
 
-(["GET" , "OPTIONS" , "HEAD" , "POST" , "PUT" , "DELETE" , "TRACE" , "CONNECT"] as const).forEach((method:TMethod) => {
-	request[method] = (api: string, data: object, loading: boolean) => {
-		return baseRequest(api, method, data, loading)
-	}
-})
+(["GET", "OPTIONS", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT"] as const)
+	.forEach((method : TMethod) => {
+		request[method] = (api : string, data : object, loading : boolean) => {
+			return baseRequest(api, method, data, loading)
+		}
+	})
 
 
 
